37e4551e0ff8df87c0a65b68f7b8b87591f6278c
Md Ashikur Rahman <draculaashik123@gmail.com>
2021-07-26 15:20:29 +0600

    GG

---
+++ js/raw.js
@@ -0,0 +1,22 @@
+
+        // const singleOptionss=document.querySelector=".single-option";
+
+
+
+// function showText(){
+//     console.log("hello");
+// document.getElementsByClassName("single-option-inner").classList.add(".single-option-inner-show");
+
+// }
+
+
+
+
+
+
+
+
+
+        function myFunction() {
+            document.documentElement.scrollTop = 0;
+        }
